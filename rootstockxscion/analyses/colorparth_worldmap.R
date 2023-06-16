f48c273e67097f3fdbdf86e0cb0a4cbd2dcb49fe
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2023-06-16 14:50:54 -0700

    Update cloropleth world map

--- rootstockxscion/analyses/colorparth_worldmap.R
+++ rootstockxscion/analyses/colorparth_worldmap.R
@@ -14,14 +14,16 @@ options(stringsAsFactors=FALSE)
 
 ### Install packages if not already in system library
 # install.packages("devtools")
+# install.packages("kaleido")
+
 library("devtools")
 # devtools::install_github("ropensci/plotly")
+library(ggplot2)
+library(dplyr)
 library(plotly)
 library(rjson)
+library(orca)
 library("RColorBrewer")
-library(tidyverse)
-library(dplyr)
-
 # Directory Lizzie
 #setwd("~/Documents/git/projects/vin/general/docs/manuscripts/asevdiversity/rootstockxscion/")
 
@@ -49,19 +51,18 @@ head(occurence)
 #### WORLD MAP ####
 
 # set boundaries to light grey  
-l <- list(color = toRGB("#D3D3D3"), width = 0.5)
+l <- list(color = "grey", width = 0.1)
 
 # specify map projection options, resolution, the scode, etc.
-
 g <- list(
   projection = list(
-    type = 'equirectangular' # the ones I like: orthographic, mercator. Source: https://plotly.com/r/map-configuration/#map-projections
+    type = 'mercator.' # the ones I like: orthographic, mercator. Source: https://plotly.com/r/map-configuration/#map-projections
   ),
   resolution = 50,
   scope = "world",
   showcoastlines=TRUE,
   showland=TRUE,
-  landcolor = toRGB("#F5F5F5")  # white smoke # for rgb colors: https://www.rapidtables.com/web/color/RGB_Color.html
+  landcolor = "white"  # white smoke # for rgb colors: https://www.rapidtables.com/web/color/RGB_Color.html
 )
 # Set country color gradient according to the number of papers in each country
 fig <- plot_geo(occurence)
@@ -69,17 +70,27 @@ fig <- fig %>% add_trace(
   z = ~count, color = ~count, colors = 'GnBu', #For color brewer palettes: http://www.sthda.com/english/wiki/colors-in-r
   text = ~COUNTRY, locations = ~code, marker = list(line = l)
 )
+
 # Add lon and lat points to the map, where each location point is related to papers title
 fig <- fig %>% add_markers(
   x = ~duse$lon, y = ~duse$lat,
-  text =~duse$title , color = I("black"), size = I(10)
+  text =~duse$title , color = I("black"), size = I(0.5)
 )
 # Set color bar
-fig <- fig %>% colorbar(title = 'Occurence', tickprefix = '')
+fig <- fig %>% colorbar(title = 'Occurence')
 # Set title
 fig <- fig %>% layout(
   title = 'Rootstock x Scion Experiment Locations',
   geo = g
 )
 fig
+
+### Install reticulate for export
+#install.packages('reticulate')
+#reticulate::install_miniconda()
+#reticulate::conda_install('r-reticulate', 'python-kaleido')
+#reticulate::conda_install('r-reticulate', 'plotly', channel = 'plotly')
+#reticulate::use_miniconda('r-reticulate')
+
+save_image(fig, "asevdiversitydup/figures/mercator_worldmap.png", scale=15)
 
