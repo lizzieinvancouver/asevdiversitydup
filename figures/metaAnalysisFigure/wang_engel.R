0919b184b87b7169bb85917aefe645e8affd37ff
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-01-24 15:00:03 -0800

    committing changes after talking to Lizzie

--- figures/metaAnalysisFigure/wang_engel.R
+++ figures/metaAnalysisFigure/wang_engel.R
@@ -56,14 +56,14 @@ for (i in 1:6) {
   
   # Plot the continuous line
   plot(
-    x, y, type = "l", lwd = 8, col = "black", xlab = "", ylab = "",
+    x, y, type = "l", lwd = 8, col = "#c701ff", xlab = "", ylab = "",
     main = "", bty = "l", xaxt = "n", yaxt = "n",
   )
   # keep ticks, remove their labels
   axis(1, labels = FALSE, tick = TRUE) 
   axis(2, labels = FALSE, tick = TRUE) 
   # Define dots with varying numbers and positions
-  n_dots <- 3 + (i %% 3)  # Alternate between 3, 4, and 5 dots
+  n_dots <- 2 + (i %% 4)  # Alternate between 3, 4, and 5 dots
   x_dots <- sort(seq(Tmin + 10, Tmax - 10, length.out = n_dots))  # Equally spaced dots
   y_dots <- sapply(x_dots, wang, Tmin = Tmin, Topt = Topt, Tmax = Tmax)
   
@@ -81,10 +81,11 @@ for (i in 1:6) {
 parameters <- list(
   list(Tmin = -40, Topt = 20, Tmax = 40), # figure 1
   list(Tmin = -20, Topt = 30, Tmax = 40), # figure 2
-  list(Tmin = -35, Topt = 20, Tmax = 40) # figure 3
+  list(Tmin = -35, Topt = 20, Tmax = 40), # figure 2
+  list(Tmin = -35, Topt = 15, Tmax = 40) # figure 3
 )
 
-for (i in 1:3) {
+for (i in 1:4) {
   # Extract parameters for this figure
   Tmin <- parameters[[i]]$Tmin
   Topt <- parameters[[i]]$Topt
@@ -130,7 +131,7 @@ parameters <- list(
 )
 
 
-# Generate and save 5 different figures
+# Generate and save 2 different figures
 for (i in 1:2) {
   # Extract parameters for this figure
   Tmin <- parameters[[i]]$Tmin
@@ -165,10 +166,11 @@ for (i in 1:2) {
 # === === === === === === === === === === === === === === ===
 parameters <- list(
   list(Tmin = -10, Topt = 30, Tmax = 40), # figure 1
-  list(Tmin = -40, Topt = 20, Tmax = 40) # figure 2
+  list(Tmin = -40, Topt = 20, Tmax = 40), # figure 2
+  list(Tmin = -20, Topt = 25, Tmax = 40) # figure 3
 )
 
-for (i in 1:2) {
+for (i in 1:3) {
   # Extract parameters for this figure
   Tmin <- parameters[[i]]$Tmin
   Topt <- parameters[[i]]$Topt
@@ -193,7 +195,7 @@ for (i in 1:2) {
   axis(1, labels = FALSE, tick = TRUE) 
   axis(2, labels = FALSE, tick = TRUE) 
   # Define dots with varying numbers and positions
-  n_dots <- 3 + (i %% 3)  # Alternate between 3, 4, and 5 dots
+  n_dots <- 2 + (i %% 2)  # Alternate between 3, 4, and 5 dots
   x_dots <- sort(seq(Tmin + 10, Tmax - 10, length.out = n_dots))  # Equally spaced dots
   y_dots <- sapply(x_dots, wang, Tmin = Tmin, Topt = Topt, Tmax = Tmax)
   
@@ -249,13 +251,14 @@ for (i in 1:3) {
 par(mgp = c(1, -5, 0)) # Adjust the first value for axis title position
 
 parameters <- list(
-  list(Tmin = -40, Topt = 25, Tmax = 40), # curve rootstock 1
-  list(Tmin = -40, Topt = 30, Tmax = 40), # curve rootstock 2
-  list(Tmin = -40, Topt = 20, Tmax = 40)  # curve rootstock 3
+  list(Tmin = -40, Topt = 20, Tmax = 40), # UNIVERSAL
+  list(Tmin = -40, Topt = 30, Tmax = 40), # curve rootstock 1
+  list(Tmin = -40, Topt = 25, Tmax = 40), # curve rootstock 2
+  list(Tmin = -40, Topt = 10, Tmax = 40) # curve rootstock 3
 )
-colors <- c("black", "#c83034", "#003a7d")
-line_styles <- c(1, 2, 2) # Specify line styles for each curve
-
+colors <- c("black", "#c701ff", "#c83034", "#003a7d")
+line_styles <- c(1, 2, 2, 2) # Specify line styles for each curve
+line_width <- c(3,2,2,2)
 # Open a single PDF device
 pdf("universalcurves.pdf", width = 6, height = 8)
 
@@ -271,7 +274,7 @@ axis(1, labels = FALSE, tick = TRUE) # x-axis with ticks but no tick labels
 axis(2, labels = FALSE, tick = TRUE) # y-axis with ticks but no tick labels
 
 # Loop through each parameter set to add curves to the same plot
-for (i in 1:3) {
+for (i in 1:4) {
   # Extract parameters for this curve
   Tmin <- parameters[[i]]$Tmin
   Topt <- parameters[[i]]$Topt
@@ -281,7 +284,7 @@ for (i in 1:3) {
   y <- sapply(x, wang, Tmin = Tmin, Topt = Topt, Tmax = Tmax)
   
   # Add the curve to the plot with specified color and line style
-  lines(x, y, lwd = 3, lty = line_styles[i], col = colors[i])
+  lines(x, y, lwd = line_width[i], lty = line_styles[i], col = colors[i])
 }
 
 # Close the PDF device
